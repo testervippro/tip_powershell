f4286efd15b06097aaf2bbd9bd2371f9256f322b
testervippro <92687973+testervippro@users.noreply.github.com>
2024-12-20 21:19:00 +0700

    Update jenkin.ps1

--- jenkin.ps1
+++ jenkin.ps1
@@ -8,5 +8,21 @@ Invoke-WebRequest -Uri $JenkinsURL -OutFile $DownloadPath -UseBasicParsing
 # Start Jenkins with admin credentials
 Start-Process "java" -ArgumentList "-jar `"$DownloadPath`" --argumentsRealm.passwd.admin=admin --argumentsRealm.roles.admin=admin" -NoNewWindow
 
-# Use Invoke-WebRequest to open Jenkins setup page
-Invoke-WebRequest -Uri "http://localhost:8080" -UseBasicParsing
+# Wait for Jenkins to start and open the setup page
+$JenkinsReady = $false
+$MaxWaitTime = 300  # Maximum wait time in seconds (5 minutes)
+$StartTime = (Get-Date).Ticks
+
+Do {
+    Start-Sleep -Seconds 10  # Check every 10 seconds
+    $JenkinsReady = (Test-Connection -ComputerName localhost -Port 8080 -Count 1 -ErrorAction SilentlyContinue)
+
+    $CurrentTime = (Get-Date).Ticks
+    $ElapsedTime = [math]::Floor(($CurrentTime - $StartTime) / [timespan]::FromSeconds(1).Ticks)
+} until ($JenkinsReady -or $ElapsedTime -ge $MaxWaitTime)
+
+if ($JenkinsReady) {
+    Invoke-WebRequest -Uri "http://localhost:8080" -UseBasicParsing
+} else {
+    Write-Host "Jenkins did not start within the expected time frame."
+}
